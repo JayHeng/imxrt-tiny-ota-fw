3a225a59d697da0f9aaf5442d2257543dbfc25fb
Jay Heng <hengjie1989@foxmail.com>
2026-01-15 14:36:23 +0800

    [RT1170][TAPP] Can print app header info
    - No FCB
    - Vector 0x30002000 -> 0x30080000
    
    Signed-off-by: Jay Heng <hengjie1989@foxmail.com>

--- targets/imxrt1170/apps/tota_app/cm7/tota_app.c
+++ targets/imxrt1170/apps/tota_app/cm7/tota_app.c
@@ -11,9 +11,26 @@
 #include "board.h"
 #include "app.h"
 
+#if (defined(__ICCARM__))
+#pragma section = ".intvec"
+#endif
+
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
+
+#define APP_MAGIC (0x50504154) //'TAPP'
+
+typedef struct _tota_app_header
+{
+    uint32_t reserved0[8];
+    uint32_t length;
+    uint16_t version;
+    uint16_t authType;
+    uint32_t checksum;
+    uint32_t reserved1[2];
+    uint32_t magic;
+} tota_app_header_t;
 
 /*******************************************************************************
  * Prototypes
@@ -39,6 +56,21 @@ int main(void)
     PRINTF("MCUX SDK version: %s\r\n", MCUXSDK_VERSION_FULL_STR);
 
     PRINTF("hello Tiny OTA app.\r\n");
+
+#if (defined(__ICCARM__))
+    uint32_t vectorStart = (uint32_t)__section_begin(".intvec");
+    PRINTF("app vector addr = 0x%x.\r\n", vectorStart);
+    tota_app_header_t *appHeader = (tota_app_header_t *)vectorStart;
+    if (appHeader->magic == APP_MAGIC)
+    {
+        PRINTF("app version: V%d.%d \r\n", appHeader->version >>8, appHeader->version & 0xFF);
+        PRINTF("app length (bytes) = 0x%x.\r\n", appHeader->length);
+    }
+    else
+    {
+        PRINTF("app doesn't contain magic.\r\n");
+    }
+#endif
 
     while (1)
     {
